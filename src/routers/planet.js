144368b9a50ac1391684e3818fdaf79a0282d81d
gladson <gladsonbrito@gmail.com>
2020-10-11 22:39:00 -0400

    refactor(routers): add http - patch

--- src/routers/planet.js
+++ src/routers/planet.js
@@ -11,98 +11,132 @@ const router = express.Router();
  */
 
 router.get("/", async (req, res) => {
-  try {
-    const pageSize = parseInt(req.query.pageSize, 10) || 10;
-    const pageNumber = parseInt(req.query.pageNumber, 10) || 1;
+    try {
+        const pageSize = parseInt(req.query.pageSize, 10) || 10;
+        const pageNumber = parseInt(req.query.pageNumber, 10) || 1;
 
-    const options = {
-      limit: pageSize,
-      page: pageNumber,
-      sort: "-createdAt",
-    };
+        const options = {
+            limit: pageSize,
+            page: pageNumber,
+            sort: "-createdAt",
+        };
 
-    Object.keys(req.query).forEach((key) => {
-      if (key === "pageNumber" || key === "pageSize") {
-        delete req.query[key];
-      }
-    });
+        Object.keys(req.query).forEach((key) => {
+            if (key === "pageNumber" || key === "pageSize") {
+                delete req.query[key];
+            }
+        });
 
-    const query = Object.keys(req.query).reduce((mappedQuery, key) => {
-      if (req.query[key]) {
-        mappedQuery[key] =
-          typeof req.query[key] === "boolean"
-            ? req.query[key]
-            : new RegExp(req.query[key], "i");
-      }
-      return mappedQuery;
-    }, {});
+        const query = Object.keys(req.query).reduce((mappedQuery, key) => {
+            if (req.query[key]) {
+                mappedQuery[key] =
+                    typeof req.query[key] === "boolean"
+                        ? req.query[key]
+                        : new RegExp(req.query[key], "i");
+            }
+            return mappedQuery;
+        }, {});
 
-    const planets = await Planet.paginate(query, options);
-    res.status(200).json(planets);
-  } catch (error) {
-    if (process.env.DEBUG === true) {
-      res.status(500).json(`Error: ${error}`);
-    } else {
-      res.status(500).json({
-        success: false,
-        message: "Error: Desculpe ocorreu um problema",
-      });
+        const planets = await Planet.paginate(query, options);
+        res.status(200).json(planets);
+    } catch (error) {
+        if (process.env.DEBUG === true) {
+            res.status(500).json(`Error: ${error}`);
+        } else {
+            res.status(500).json({
+                success: false,
+                message: "Error: Desculpe ocorreu um problema",
+            });
+        }
     }
-  }
 });
 
-router.post("/", validate.validatePlanet(), async (req, res) => {
-  try {
-    const errors = validationResult(req);
+router.post("/", validate.validatePostPlanet(), async (req, res) => {
+    try {
+        const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      res.status(422).json({
-        success: false,
-        message: errors,
-      });
+        if (!errors.isEmpty()) {
+            res.status(422).json({
+                success: false,
+                message: errors,
+            });
+        }
+
+        const planet = new Planet({
+            name: req.body.name,
+            climate: req.body.climate,
+            ground: req.body.ground,
+        });
+
+        const resultPlanet = await planet.save();
+        res.json(resultPlanet);
+    } catch (error) {
+        if (process.env.DEBUG === true) {
+            res.status(500).json(`Error: ${error}`);
+        } else {
+            res.status(500).json({
+                success: false,
+                message: "Error: Desculpe ocorreu um problema",
+            });
+        }
     }
-
-    const planet = new Planet({
-      name: req.body.name,
-      climate: req.body.climate,
-      ground: req.body.ground,
-    });
-
-    const resultPlanet = await planet.save();
-    res.json(resultPlanet);
-  } catch (error) {
-    if (process.env.DEBUG === true) {
-      res.status(500).json(`Error: ${error}`);
-    } else {
-      res.status(500).json({
-        success: false,
-        message: "Error: Desculpe ocorreu um problema",
-      });
-    }
-  }
 });
 
 router.get("/:id", async (req, res) => {
-  try {
-    const planet = await Planet.findById(req.params.id);
-    if (planet === null) {
-      res.status(404).json({
-        success: false,
-        message:
-          "Error: Desculpe não conseguimos encontrar o planeta em nossa base dados",
-      });
+    try {
+        const planet = await Planet.findById(req.params.id);
+        if (planet === null) {
+            res.status(404).json({
+                success: false,
+                message:
+                    "Error: Desculpe não conseguimos encontrar o planeta em nossa base dados",
+            });
+        }
+        res.json(planet);
+    } catch (error) {
+        if (process.env.DEBUG === true) {
+            res.status(500).json(`Error: ${error}`);
+        } else {
+            res.status(500).json({
+                success: false,
+                message: "Error: Desculpe ocorreu um problema",
+            });
+        }
     }
-    res.json(planet);
-  } catch (error) {
-    if (process.env.DEBUG === true) {
-      res.status(500).json(`Error: ${error}`);
-    } else {
-      res.status(500).json({
-        success: false,
-        message: "Error: Desculpe ocorreu um problema",
-      });
+});
+
+router.patch("/:id", validate.validatePatchPlanet(), async (req, res) => {
+    try {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.status(422).json({
+                success: false,
+                message: errors,
+            });
+        }
+
+        const planet = await Planet.findById(req.params.id);
+        if (planet === null) {
+            res.status(404).json({
+                success: false,
+                message:
+                    "Error: Desculpe não conseguimos encontrar em nossa base dados",
+            });
+        }
+        planet.ground = req.body.ground;
+        const resultChangePlanet = await planet.save();
+        res.json(resultChangePlanet);
+    } catch (error) {
+        if (process.env.DEBUG === true) {
+            res.status(500).json(`Error: ${error}`);
+        } else {
+            res.status(500).json({
+                success: false,
+                message: "Error: Desculpe ocorreu um problema",
+            });
+        }
     }
-  }
 });
 
 module.exports = router;
