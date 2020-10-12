820d9a88ad3e8739d844cd8352ba9cc6586cb62a
gladson <gladsonbrito@gmail.com>
2020-10-12 01:56:57 -0400

    refactor(app): fechar servidor caso nao seja configurado corretamente o .env

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 require("dotenv").config();
-
 const express = require("express");
 const mongoose = require("mongoose");
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect(process.env.MONGOURI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose
+    .connect(process.env.MONGOURI, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() => {})
+    .catch((err) => {
+        console.log(
+            "Por favor efetue a configuração correta do arquivo .env..."
+        );
+        server.close(function () {
+            process.exit(1);
+        });
+    });
 
 const con = mongoose.connection;
 
 con.on("open", () => {
-  console.log("MongoDB conectado...");
+    console.log("MongoDB conectado...");
 });
 
 // ROUTE
@@ -30,5 +41,5 @@ const port = process.env.PORT || 9000;
 const host = process.env.HOST || "http://localhost";
 
 app.listen(port, () => {
-  console.log(`Servidor rodando => ${host}:${port}`);
+    console.log(`Servidor rodando => ${host}:${port}`);
 });
