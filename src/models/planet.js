cf70d5cb83e7ca06874885b56197eb7e48bc410b
gladson <gladsonbrito@gmail.com>
2020-10-11 22:01:55 -0400

    add(models): planet
    * com paginação

---
+++ src/models/planet.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+
+/**
+ * Model => planetSchema
+ *
+ * | RDBMS    | MONGODB     |
+ * |----------|-------------|
+ * | Database | Database    |
+ * | Tables   | Collections |
+ * | Rows     | Documents   |
+ * | Columns  | Fields      |
+ *
+ */
+
+/**
+ * planetSchema:
+ *
+ * -> Id: int
+ * -> Name: String
+ * -> Climate: String
+ * -> Ground: String
+ *
+ */
+
+const planetSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    climate: {
+      type: String,
+      required: true,
+    },
+    ground: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+planetSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("Planet", planetSchema);
