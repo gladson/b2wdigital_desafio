e7f10294dd78ac5e75f0ff58dc7c675b3cf2aff4
gladson <gladsonbrito@gmail.com>
2020-10-11 23:00:27 -0400

    refactor(validate): alterar validatePlanet para validatePostPutPlanet
    * e criar um novo para o http patch validatePatchPlanet

--- src/validators/validate.js
+++ src/validators/validate.js
@@ -1,25 +1,34 @@
 const { check } = require("express-validator");
 
-const validatePlanet = () => [
-  check("name")
-    .notEmpty()
-    .withMessage("Name not empty")
-    .isString()
-    .withMessage("Name must be String"),
-  check("climate")
-    .notEmpty()
-    .withMessage("Climate not empty")
-    .isString()
-    .withMessage("Climate must be String"),
-  check("ground")
-    .notEmpty()
-    .withMessage("Ground not empty")
-    .isString()
-    .withMessage("Ground must be String"),
+const validatePostPutPlanet = () => [
+    check("name")
+        .notEmpty()
+        .withMessage("Name not empty")
+        .isString()
+        .withMessage("Name must be String"),
+    check("climate")
+        .notEmpty()
+        .withMessage("Climate not empty")
+        .isString()
+        .withMessage("Climate must be String"),
+    check("ground")
+        .notEmpty()
+        .withMessage("Ground not empty")
+        .isString()
+        .withMessage("Ground must be String"),
+];
+
+const validatePatchPlanet = () => [
+    check("ground")
+        .notEmpty()
+        .withMessage("Ground not empty")
+        .isString()
+        .withMessage("Ground must be String"),
 ];
 
 const validator = {
-  validatePlanet,
+    validatePostPutPlanet,
+    validatePatchPlanet,
 };
 
 module.exports = validator;
